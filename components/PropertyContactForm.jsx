c174791aaa2920cc22dd6c2c7c7e02296cec15f3
munish91 <munishkrsinghh91@gmail.com>
2025-03-01 20:53:48 +0530

    message update

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -1,10 +1,39 @@
+'use client'
+import { useEffect } from "react"
+import { useFormState,useFormStatus } from "react-dom"
+import { useSession } from "next-auth/react"
+import {toast} from 'react-toastify'
+import addMessage from "@/app/actions/addMessage"
 import { FaPaperPlane } from "react-icons/fa"
 
 const PropertyContactForm = ({property}) => {
-  return (
+  const {data:session}= useSession();
+  const [state,formAction] = useFormState(addMessage,{})
+
+  useEffect(()=>{
+    if(state.error) toast.error(state.error)
+    if(state.submitted) toast.success('Message sent successfully')
+  },[state])
+  
+   if(state.submitted){
+    return <p className="text-green-500 mb-">Your message has been sent</p>
+   }
+  return session &&(
     <div className="bg-white p-6 rounded-lg shadow-md">
               <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
-              <form>
+              <form action={formAction}>
+                <input 
+                type="hidden"
+                id="property"
+                name="property"
+                defaultValue={property._id}
+                />
+                <input 
+                type="hidden"
+                id="recipient"
+                name="recipient"
+                defaultValue={property.owner}
+                />
               <div className='mb-4'>
                 <label
                   className='block text-gray-700 text-sm font-bold mb-2'
@@ -15,6 +44,7 @@ const PropertyContactForm = ({property}) => {
                 <input
                   className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                   id='name'
+                  name='name'
                   type='text'
                   placeholder='Enter your name'             
                   required
@@ -31,6 +61,7 @@ const PropertyContactForm = ({property}) => {
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="email"
                     type="email"
+                    name="email"
                     placeholder="Enter your email"
                     required
                   />
@@ -52,13 +83,14 @@ const PropertyContactForm = ({property}) => {
                 <div className="mb-4">
                   <label
                     className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="message"
+                    htmlFor="body"
                   >
                     Message:
                   </label>
                   <textarea
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-                    id="message"
+                    id="body"
+                    name='body'
                     placeholder="Enter your message"
                   ></textarea>
                 </div>
